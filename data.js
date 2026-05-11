ab4c705cfba684e1535593459c0d7211edcd930a
saquitodemirra836-ai <saquitodemirra836@icloud.com>
2026-05-11 16:19:49 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,40 @@
+// USUARIOS
+const usuarios = [
+  "Alba", "Carlos", "Lucía", "Jon", "Fátima",
+  "Hugo", "Marcos", "Sara", "Valeria", "Nerea"
+];
+
+// GRUPOS
+const grupos = {
+  "Grupo Azul": ["Alba", "Carlos", "Lucía", "Jon"],
+  "Grupo Verde": ["Fátima", "Hugo", "Marcos"],
+  "Grupo Amarillo": ["Sara", "Valeria", "Nerea"]
+};
+
+// ZONAS + pictograma ARASAAC
+const zonas = {
+  "Comedor": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/3738/3738.png)",
+    tareas: ["Barrer", "Fregar", "Limpiar polvo"]
+  },
+  "Cocina": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/15929/15929.png)",
+    tareas: ["Barrer", "Fregar", "Quitar polvo", "Ventanas"]
+  },
+  "Pasillos": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/6966/6966.png)",
+    tareas: ["Barrer", "Fregar", "Rodapiés"]
+  },
+  "Biblioteca": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/5085/5085.png)",
+    tareas: ["Quitar polvo", "Limpiar polvo", "Ventanas"]
+  },
+  "Gimnasio": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/11261/11261.png)",
+    tareas: ["Barrer", "Fregar"]
+  },
+  "Baños": {
+    picto: "[static.arasaac.org](https://static.arasaac.org/pictograms/18893/18893.png)",
+    tareas: ["Barrer", "Fregar"]
+  }
+};
